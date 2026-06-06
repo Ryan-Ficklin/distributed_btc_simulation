876720c83b8d8fb0013211d82bf79d113344660e
Ryan Ficklin <ryanpficklin@gmail.com>
2026-06-06 16:10:32 -0700

    contains

--- client.go
+++ client.go
@@ -443,6 +443,34 @@ func check_difficulty(pow []byte, leading uint) bool {
 	}
 }
 
+
+func tx_contains(lst []shared.Transaction, item shared.Transaction) bool {
+  for _, tx := range lst {
+    // signature the same
+    sig_same := bytes.Equal(tx.Signature, item.Signature)
+    // input the same
+    input_same := bytes.Equal(tx.Input.Block_ID, item.Input.Block_ID) &&
+                  tx.Input.N == item.Input.N
+
+    // output the same 
+    output_same := true
+    if len(tx.Output) == len(item.Output) {
+      // check if each output is equal  
+      for i, o := range tx.Output {
+        output_same = output_same && o.Value == item.Output[i].Value 
+                                   && bytes.Equal(o.PubKey, item.Output[i].PubKey)
+      }
+    } else {
+      output_same = false
+    }
+
+    if output_same && input_same && sig_same {
+      return true
+    }
+  }
+  return false
+}
+
 // ~~~~~~~~ GOSSIP HB PROTOCOL ~~~~~~~~~~~~
 
 // Send the current membership table to a neighboring node with the provided ID
